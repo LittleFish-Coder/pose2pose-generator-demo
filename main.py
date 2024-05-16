b60a1d3cf786128c0c603ad99158c1d5a2862662
Hunk0724 <hunk123321123@gmail.com>
2024-05-16 20:03:46 +0800

    conf 0.7 add hand color

--- main.py
+++ main.py
@@ -56,8 +56,9 @@ with col3:
                 frame_number = 0
                 mp_holistic = mp.solutions.holistic             # mediapipe 全身偵測方法
                 mp_hands = mp.solutions.hands
-
-                with mp_holistic.Holistic(min_detection_confidence=0.6, min_tracking_confidence=0.6) as holistic:
+                # 自定義手部landmark的顏色和粗細
+                hand_landmark_style = mp_drawing.DrawingSpec(color=(0,255,0), thickness=2)
+                with mp_holistic.Holistic(min_detection_confidence=0.7, min_tracking_confidence=0.7) as holistic:
     
                     while cap.isOpened():
                         ret, frame = cap.read()
@@ -73,10 +74,10 @@ with col3:
                         # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
                         
                         # Right hand
-                        mp_drawing.draw_landmarks(black_background, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                        mp_drawing.draw_landmarks(black_background, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,landmark_drawing_spec=hand_landmark_style)
 
                         # Left Hand
-                        mp_drawing.draw_landmarks(black_background, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                        mp_drawing.draw_landmarks(black_background, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,landmark_drawing_spec=hand_landmark_style)
 
                         # Pose Detections
                         mp_drawing.draw_landmarks(black_background, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
