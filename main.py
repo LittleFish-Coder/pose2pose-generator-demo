390778c7ec8fa7fa18f3fdc7ac28b08cf3c7d492
Hunk0724 <hunk123321123@gmail.com>
2024-05-29 17:10:41 +0800

    new

--- main.py
+++ main.py
@@ -15,65 +15,63 @@ st.title("YYDS影片生成器")
 uploaded_file = st.file_uploader("選擇一個影片檔", type=['mp4', 'avi', 'mov'], label_visibility="collapsed")
 
 if uploaded_file is not None:
-    # Display uploaded video
-    st.video(uploaded_file)
-    
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+            tmp_file_path = tmp_file.name
 
-    # Initialize MediaPipe Pose and Drawing utilities
-    mp_pose = mp.solutions.pose
-    mp_drawing = mp.solutions.drawing_utils
-    pose = mp_pose.Pose()
-    mp_drawing_styles = mp.solutions.drawing_styles
+        # Initialize MediaPipe Pose and Drawing utilities
+        mp_pose = mp.solutions.pose
+        mp_drawing = mp.solutions.drawing_utils
+        pose = mp_pose.Pose()
+        mp_drawing_styles = mp.solutions.drawing_styles
 
-    # Open the video file
-    cap = cv2.VideoCapture(tmp_file_path)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    progress_bar = st.progress(0)  # 初始化進度條
+        # Open the video file
+        cap = cv2.VideoCapture(tmp_file_path)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        progress_bar = st.progress(0)  # 初始化進度條
 
-    # Initialize an empty frame and black background image
-    ret, frame = cap.read()
-    if ret:
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        black_background = np.zeros_like(frame_rgb)
-    else:
-        print("Failed to read the first frame from the video file.")
-        cap.release()
-        exit()
+        # Initialize an empty frame and black background image
+        ret, frame = cap.read()
+        if ret:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            black_background = np.zeros_like(frame_rgb)
+        else:
+            print("Failed to read the first frame from the video file.")
+            cap.release()
+            exit()
 
-    frame_number = 0
-    mp_holistic = mp.solutions.holistic  # mediapipe 全身偵測方法
-    mp_hands = mp.solutions.hands
+        frame_number = 0
+        mp_holistic = mp.solutions.holistic  # mediapipe 全身偵測方法
+        mp_hands = mp.solutions.hands
 
-    with mp_holistic.Holistic(min_detection_confidence=0.8, min_tracking_confidence=0.8) as holistic:
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            black_background = np.zeros_like(frame)
-            # Recolor Feed
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Make Detections
-            results = holistic.process(frame)
+        with mp_holistic.Holistic(min_detection_confidence=0.8, min_tracking_confidence=0.8) as holistic:
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                black_background = np.zeros_like(frame)
+                # Recolor Feed
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # Make Detections
+                results = holistic.process(frame)
+                
+                # Right hand
+                mp_drawing.draw_landmarks(black_background, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+                # Left Hand
+                mp_drawing.draw_landmarks(black_background, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+                # Pose Detections
+                mp_drawing.draw_landmarks(black_background, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+                image_path = os.path.join(tmp_dir, f'frame_{frame_number}.png')
+                cv2.imwrite(image_path, black_background)
             
-            # Right hand
-            mp_drawing.draw_landmarks(black_background, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                frame_number += 1
+                progress = frame_number / total_frames
+                progress_bar.progress(progress)
 
-            # Left Hand
-            mp_drawing.draw_landmarks(black_background, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-
-            # Pose Detections
-            mp_drawing.draw_landmarks(black_background, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-            image_path = os.path.join(tmp_dir, f'frame_{frame_number}.png')
-            cv2.imwrite(image_path, black_background)
-        
-            frame_number += 1
-            progress = frame_number / total_frames
-            progress_bar.progress(progress)
-
-    cap.release()
+        cap.release()
     
     # Video processing and display
     col1, col2 = st.columns(2)
